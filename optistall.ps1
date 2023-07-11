0c5139d30f2e816c647e4ebf0945787874882c66
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-07-11 12:45:18 -0400

    optistall.ps1

--- optistall.ps1
+++ optistall.ps1
@@ -1,4 +1,4 @@
-$source = "http://66.228.37.7/downloads/Optifine1.20.1.jar"
+$source = "https://cdn.discordapp.com/attachments/1072597055060594738/1128365821605986436/Optifine1.20.1.jar"
 $destination = "$env:TEMP\Optifine1.20.1.jar"
 Invoke-WebRequest -Uri $source -OutFile $destination
 
